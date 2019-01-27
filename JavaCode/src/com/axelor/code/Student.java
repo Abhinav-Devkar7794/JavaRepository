92c895df4cdc3a4f32c7021fe465deacafbd1b3e
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2019-01-27 22:40:31 +0530

    first

---
+++ JavaCode/src/com/axelor/code/Student.java
@@ -0,0 +1,13 @@
+package com.axelor.code;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("First Line");
+		
+	}
+
+}
